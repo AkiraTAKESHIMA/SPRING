d7d640a494ea00e3070fb208f5064e481683b97a
Akira Takeshima <akira@rainbow.iis.u-tokyo.ac.jp>
2025-07-19 19:04:59 +0900

    Change the structure

---
+++ run/ILS_bnd/sample/run_all.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+sys.path.append('../../../src/run/common')
+sys.path.append('script')
+from script import s01_make_cmf_mat, \
+                   s02_make_rt
+
+
+#s01_make_cmf_mat.run()
+
+s02_make_rt.run()
+
