73e39413a824249a6c3f640fdc6b035fdd0f3349
Akira Takeshima <akira@rainbow.iis.u-tokyo.ac.jp>
2025-07-20 22:46:24 +0900

    rt_ILS_default

--- run/ILS_bnd/sample/run_all.py
+++ run/ILS_bnd/sample/run_all.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 
 sys.path.append('../../../src/run/common')
 sys.path.append('script')
@@ -7,7 +8,22 @@ from script import s01_make_cmf_mat, \
                    s02_make_rt
 
 
-#s01_make_cmf_mat.run()
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--step', help='step number', type=int)
+parser.add_argument('-d', '--data', help='data name')
+parser.add_argument('-l', '--land', help='land type')
+parser.add_argument('-t', '--tile', help='tile name')
+args = parser.parse_args()
 
-s02_make_rt.run()
+if args.step is None:
+    s01_make_cmf_mat.run()
+
+    s02_make_rt.run()
+
+else:
+    if args.step == 1:
+        s01_make_cmf_mat.run()
+
+    elif args.step == 2:
+        s02_make_rt.run(args.data, args.land, args.tile)
 
--- src/run/rt_ILS_default/s02_make_rt.py
+++ src/run/rt_ILS_default/s02_make_rt.py
@@ -10,7 +10,8 @@ import s00_const as lconst
 import s00_util as lutil
 
 
-def make_rt(cnf, srcMeshNameFmt, tgtMeshNameFmt, lst_landType,
+def make_rt(cnf, step, 
+            srcMeshNameFmt, tgtMeshNameFmt, lst_landType,
             use_grdara_src, use_grdara_tgt, runname_out):
     for landType in lst_landType:
         srcMeshName = srcMeshNameFmt.format(landType=landType)
@@ -38,8 +39,8 @@ def make_rt(cnf, srcMeshNameFmt, tgtMeshNameFmt, lst_landType,
         util.make_slink(f'{dir_tmp}', f'{const.dir_out}/{runname_out}')
 
 
-if __name__ == '__main__':
-    step = int(sys.argv[0][1:3])
+def run():
+    step = int(__name__.split('.')[-1][1:3])
 
     cnf = json.load(open('conf.json','r'))
     lutil.adjust_config(cnf)
@@ -49,46 +50,54 @@ if __name__ == '__main__':
     os.makedirs(lconst.dir_log[step], exist_ok=True)
 
     # IO_bnd to MATSIRO
-    make_rt(cnf, 'IO_MATSIRO_bnd_{landType}', 'MATSIRO_bnd_{landType}', 
+    make_rt(cnf, step, 
+            'IO_MATSIRO_bnd_{landType}', 'MATSIRO_bnd_{landType}', 
             ['river', 'noriv'], 
             False, False, None)
 
     # IO_met to MATSIRO
-    make_rt(cnf, 'IO_met', 'MATSIRO_{landType}', 
+    make_rt(cnf, step, 
+            'IO_met', 'MATSIRO_{landType}', 
             ['river', 'noriv'],
             False, False, None)
 
     # IO_metnc to MATSIRO
-    make_rt(cnf, 'IO_metnc', 'MATSIRO_{landType}',
+    make_rt(cnf, step, 
+            'IO_metnc', 'MATSIRO_{landType}',
             ['river', 'noriv'],
             False, False, None)
 
     # MATSIRO to CMF
     # *** Values of "area.bin" is incorrect but not used for remapping
-    make_rt(cnf, 'MATSIRO_latlon_{landType}', 'CaMa-Flood_latlon_{landType}',
+    make_rt(cnf, step, 
+            'MATSIRO_latlon_{landType}', 'CaMa-Flood_latlon_{landType}',
             ['river'],
             False, False, 'MATSIRO_to_CaMa-Flood')
 
     # CaMa-Flood to MATSIRO
     # *** Values of "area.bin" is incorrect but not used for remapping
-    make_rt(cnf, 'CaMa-Flood_latlon_{landType}', 'MATSIRO_latlon_{landType}',
+    make_rt(cnf, step, 
+            'CaMa-Flood_latlon_{landType}', 'MATSIRO_latlon_{landType}',
             ['river'],
             False, False, 'CaMa-Flood_to_MATSIRO')
 
     # MATSIRO to IO_row
     # *** Verification data for source grid is incorrect
-    make_rt(cnf, 'MATSIRO_latlon_{landType}', 'IO_MATSIRO_row_{landType}',
+    make_rt(cnf, step, 
+            'MATSIRO_latlon_{landType}', 'IO_MATSIRO_row_{landType}',
             ['river', 'noriv'],
             False, True, None)
 
     # MATSIRO to IO_latlon
-    make_rt(cnf, 'MATSIRO_{landType}', 'IO_latlon',
+    make_rt(cnf, step, 
+            'MATSIRO_{landType}', 'IO_latlon',
             ['river', 'noriv'],
             False, False, None)
 
     # CaMa-Flood to IO_row
     # *** Verification data for source grid is incorrect
-    make_rt(cnf, 'CaMa-Flood_latlon_{landType}', 'IO_CaMa-Flood_row_{landType}',
+    make_rt(cnf, step, 
+            'CaMa-Flood_latlon_{landType}', 'IO_CaMa-Flood_row_{landType}',
             ['river'],
             False, True, 'CaMa-Flood_to_IO_row')
 
