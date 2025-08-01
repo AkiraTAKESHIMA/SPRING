ed011d16e29ef0953407fea18f2e28fa6aac5450
Akira Takeshima <akira@rainbow.iis.u-tokyo.ac.jp>
2025-08-01 20:11:21 +0900

    back

--- src/run/ILS_bnd/run_all.py
+++ src/run/ILS_bnd/run_all.py
@@ -8,10 +8,7 @@ sys.path.append('../../../src/run/ILS_bnd')
 import const, util, conf
 from util import istep
 
-import s___const as lconst
-import s00_preprocess, \
-       s01_make_cmf_mat, \
-       s02_make_rt
+import s00_const as lconst
 
 
 parser = argparse.ArgumentParser()
@@ -31,9 +28,9 @@ util.env.put_f_cnf(args.config)
 if args.step is None:
     for step in range(lconst.step_max+1):
         if step == istep('make_rt'):
-            scripts[step].run(update_data, args.data, args.land, args.tile)
+            lconst.scripts[step].run(update_data, args.data, args.land, args.tile)
         else:
-            scripts[step].run(update_data)
+            lconst.scripts[step].run(update_data)
 
 else:
     if args.step > lconst.step_max:
@@ -41,11 +38,11 @@ else:
 
     for step in range(args.step):
         if step == istep('make_rt'):
-            scripts[step].run(False, args.data, args.land, args.tile)
+            lconst.scripts[step].run(False, args.data, args.land, args.tile)
         else:
-            scripts[step].run(False)
+            lconst.scripts[step].run(False)
     if args.step == istep('make_rt'):
-        scripts[args.step].run(update_data, args.data, args.land, args.tile)
+        lconst.scripts[args.step].run(update_data, args.data, args.land, args.tile)
     else:
-        scripts[args.step].run(update_data)
+        lconst.scripts[args.step].run(update_data)
 
--- src/run/ILS_bnd/s00_const.py
+++ src/run/ILS_bnd/s00_const.py
@@ -3,11 +3,23 @@ import sys
 
 import const, util
 
+import s00_const as lconst
+import s00_preprocess, \
+       s01_make_cmf_mat, \
+       s02_make_rt
+
 job = {
   0: 'preprocess', 
   1: 'make_cmf_mat',
   2: 'make_rt',
 }
+
+scripts = {
+  0: s00_preprocess,
+  1: s01_make_cmf_mat,
+  2: s02_make_rt,
+}
+step_max = len(scripts.keys())-1
 
 dir_set = util.set_dir(const.dir_set, job)
 dir_tmp = util.set_dir(const.dir_tmp, job)
